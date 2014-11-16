875509b4656b3d67cc32831c8685262fba227e5b
johnl <johnl@johnnylee.orgm>
2014-11-16 10:16:29 -0600

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+
+import os
+import sys
+
+DISTNAME            = 'scikits.pulsefit'
+DESCRIPTION         = 'Scikits pulse-fitting package.'
+LONG_DESCRIPTION    = """
+Pulse-fitting library for identifying the positions and amplitudes of
+a characteristic pulse shape.
+"""
+MAINTAINER          = 'J. David Lee',
+MAINTAINER_EMAIL    = 'johnl@crumpington.com',
+URL                 = 'http://projects.scipy.org/scipy/scikits'
+LICENSE             = 'MIT'
+DOWNLOAD_URL        = URL
+VERSION             = '0.1.1'
+
+import setuptools
+from numpy.distutils.core import setup, Extension
+
+def configuration(parent_package='', top_path=None, package_name=DISTNAME):
+    if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(package_name, parent_package, top_path,
+                           version = VERSION,
+                           maintainer  = MAINTAINER,
+                           maintainer_email = MAINTAINER_EMAIL,
+                           description = DESCRIPTION,
+                           license = LICENSE,
+                           url = URL,
+                           download_url = DOWNLOAD_URL,
+                           long_description = LONG_DESCRIPTION)
+
+    return config
+
+if __name__ == "__main__":
+    setup(configuration = configuration,
+          install_requires = 'numpy',
+          namespace_packages = ['scikits'],
+          packages = setuptools.find_packages(),
+          ext_modules = [
+              Extension(
+                  'scikits.pulsefit.blockident_median_c', 
+                  ['src/blockident_median.c']),
+              Extension(
+                  'scikits.pulsefit.ampfit_mle_c', 
+                  ['src/ampfit_mle.c']),
+              Extension(
+                  'scikits.pulsefit.util_c', 
+                  ['src/util.c']),
+          ],
+          include_package_data = True,
+          zip_safe = True, # the package can run out of an .egg file?
+          classifiers =
+          [ 'Development Status :: 1 - Planning',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Topic :: Scientific/Engineering'])
