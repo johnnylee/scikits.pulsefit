938def06bd603fefb8e84341122e2ae1d5d3826d
johnl <johnl@johnnylee.orgm>
2014-11-18 12:18:36 -0600

    Updated setup.py to use distutils.core.
    This will hopefully fix issues installing on windows.

--- setup.py
+++ setup.py
@@ -1,61 +1,43 @@
 #! /usr/bin/env python
 
-import os
-import sys
+from distutils.core import setup, Extension
 
-DISTNAME            = 'scikits.pulsefit'
-DESCRIPTION         = 'Scikits pulse-fitting package.'
-LONG_DESCRIPTION    = """
-Pulse-fitting library for identifying the positions and amplitudes of
-a characteristic pulse shape.
-"""
-MAINTAINER          = 'J. David Lee',
-MAINTAINER_EMAIL    = 'johnl@crumpington.com',
-URL                 = 'https://github.com/johnnylee/scikits.pulsefit'
-LICENSE             = 'MIT'
-DOWNLOAD_URL        = URL
-VERSION             = '0.1.1'
+setup(
+    name='scikits.pulsefit',
+    version='0.1.3',
+    description='Characteristic pulse fitting.',
 
-import setuptools
-from numpy.distutils.core import setup, Extension
+    author='J. David Lee',
+    author_email='johnl@crumpington.com',
 
-def configuration(parent_package='', top_path=None, package_name=DISTNAME):
-    if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+    maintainer='Crumpington Consulting LLC',
+    maintainer_email='johnl@crumpington.com',
+    
+    url='https://github.com/johnnylee/scikits.pulsefit',
+    
+    packages=['scikits/pulsefit'],
+    
+    ext_modules=[
+        Extension(
+            'scikits.pulsefit.blockident_median_c', 
+            ['src/blockident_median.c']),
+        Extension(
+            'scikits.pulsefit.ampfit_mle_c', 
+            ['src/ampfit_mle.c']),
+        Extension(
+            'scikits.pulsefit.util_c', 
+            ['src/util.c']),
+    ],
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering'
+    ],
+    
+    license='MIT'
+)
 
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(package_name, parent_package, top_path,
-                           version = VERSION,
-                           maintainer  = MAINTAINER,
-                           maintainer_email = MAINTAINER_EMAIL,
-                           description = DESCRIPTION,
-                           license = LICENSE,
-                           url = URL,
-                           download_url = DOWNLOAD_URL,
-                           long_description = LONG_DESCRIPTION)
-
-    return config
-
-if __name__ == "__main__":
-    setup(configuration = configuration,
-          install_requires = 'numpy',
-          namespace_packages = ['scikits'],
-          packages = setuptools.find_packages(),
-          ext_modules = [
-              Extension(
-                  'scikits.pulsefit.blockident_median_c', 
-                  ['src/blockident_median.c']),
-              Extension(
-                  'scikits.pulsefit.ampfit_mle_c', 
-                  ['src/ampfit_mle.c']),
-              Extension(
-                  'scikits.pulsefit.util_c', 
-                  ['src/util.c']),
-          ],
-          zip_safe = True, # the package can run out of an .egg file?
-          classifiers =
-          [ 'Development Status :: 3 - Alpha',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Topic :: Scientific/Engineering'])
